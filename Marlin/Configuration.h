a30652cbb787832a96e6e73eb458d8c6d511b19b
valemaio2 <72505627+valemaio2@users.noreply.github.com>
2021-06-30 22:02:20 +0100

    PID tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -582,9 +582,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  27.09
+    #define DEFAULT_Ki   2.39
+    #define DEFAULT_Kd 76.74
   #endif
 #endif // PIDTEMP
 
