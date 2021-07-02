eb62e8e139107d056d9057d16127b64c47d4928e
valemaio2 <72505627+valemaio2@users.noreply.github.com>
2021-07-02 01:46:25 +0100

    steppers calibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -897,7 +897,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.79, 80, 403.14, 100.79 } //E was 95
 
 /**
  * Default Max Feed Rate (mm/s)
